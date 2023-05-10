fbf4a946cc5c82921a1da71abf17ee58d017efd9
412 <laybacksound96@gmail.com>
2023-05-10 10:25:54 +0900

    section 05

--- 04/src/components/NewExpense/NewExpense.js
+++ 04/src/components/NewExpense/NewExpense.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = () => {
+  const [isOpened, setIsOpened] = useState(false);
+  const openHandler = () => {
+    setIsOpened((prev) => {
+      return !prev;
+    });
+  };
   return (
-    <div className='new-expense'>
-      <ExpenseForm />
+    <div className="new-expense">
+      {isOpened ? (
+        <ExpenseForm onOpen={openHandler} />
+      ) : (
+        <button onClick={openHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
