7a7d1a7237650eb0733a6a6d5c48ae354d44b87c
412 <85489519+laybacksound96@users.noreply.github.com>
2023-05-11 16:33:49 +0900

    스타일컴포넌트 재설치

--- 07_app_cloning/src/components/Meals/MealCard.js
+++ 07_app_cloning/src/components/Meals/MealCard.js
@@ -1,6 +1,9 @@
-import styled from "styled-component";
+import React from "react";
+import styled from "styled-components";
 
-const MealCardComponent = styled.div``;
+const MealCardComponent = styled.div`
+  background-color: blue;
+`;
 
 const MealCard = (props) => {
   return (
